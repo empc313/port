b966637a59b9367b4e1fec05b4569899da3c39f6
Emma <cemma313@gmail.com>
2024-07-18 09:39:53 -0600

    setting up header

---
+++ client/src/components/header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function Header(){
+return(
+  <header className="bg-white">
+    <div>
+      <span>
+  Emma Cunningham
+      </span>
+    </div>
+  </header>
+)
+}
